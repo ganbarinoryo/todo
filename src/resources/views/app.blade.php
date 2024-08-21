77d053393758be59f02d5283168915a2cec42479
ryo morita <epkvg5316@gmail.com>
2024-08-21 23:04:25 +0900

    todoセットアップ

---
+++ src/resources/views/app.blade.php
@@ -0,0 +1,4 @@
+@yield('index.blade.php')
+
+<link rel="stylesheet" href="{{ asset('css/common.css') }}">
+<link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('app.blade.php')
+
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
